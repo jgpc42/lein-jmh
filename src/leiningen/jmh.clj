bad4268f98332618841fbd6772cb688ceb543668
Justin Conklin <jgpc42@gmail.com>
2017-10-02 20:51:56 -0700

    refactor subprocess code into separate namespace

--- src/leiningen/jmh.clj
+++ src/leiningen/jmh.clj
@@ -2,19 +2,13 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.set :as set]
+            [leiningen.jmh.form :as form]
             [leiningen.core.eval :as eval]
             [leiningen.core.main :as main]
             [leiningen.core.project :as project]))
 
 (def ^:private version
   (-> "version.edn" io/resource slurp edn/read-string))
-
-(def ^:private task-requires
-  '[jmh.core clojure.pprint])
-
-(def ^:private init-form
-  (list* `require (map (partial list 'quote)
-                       task-requires)))
 
 (defn merge-recursively
   "Merge two like-values recursively with an appropriate combining fn.
@@ -44,98 +38,6 @@
   [project opts]
   (let [path (:compile-path project (or *compile-path* "classes"))]
     (merge {:compile-path path} opts)))
-
-(def ^{:private true
-       :doc "The code for a fn that reports progress events to the given
-            Writer object."}
-  progress-form
-  `(let [prev# (volatile! nil)]
-     (fn [out# {eta# :eta, pct# :percent}]
-       (let [line# (format "{:%% %.1f :eta \"%02d:%02d:%02d\"}"
-                           (* 100.0 pct#)
-                           (long (/ eta# (* 60.0 60)))
-                           (long (/ (mod eta# (* 60.0 60)) 60))
-                           (mod eta# 60))
-             nline# (count line#)
-             nprev# (count @prev#)
-             prev?# (pos? nprev#)
-             clear# (if (and prev?# (< nline# nprev#))
-                      (apply str (repeat (- nprev# nline#) \space))
-                      "")]
-         (binding [*out* out#]
-           (locking out#
-             (when prev?#
-               (print \return))
-             (print line#)
-             (print clear#)
-             (when (>= pct# 1.0)
-               (newline))
-             (flush)
-             (vreset! prev# line#)))))))
-
-(defn- options-form
-  "Return the code for the benchmark options map."
-  [opts]
-  `(let [opts# '~opts
-         pval# (:progress opts# :out)]
-     (cond
-       (or (#{:out :err} pval#)
-           (true? pval#))
-       (assoc opts# :progress
-              (partial ~progress-form
-                       (if (= pval# :err) *err* *out*)))
-       (not pval#)
-       opts#
-       :else
-       (update opts# :progress eval))))
-
-(defn- run-form
-  "Return the benchmarking code."
-  [project {out :output :as opts}]
-  (let [env (merge-environment project (:file opts "jmh.edn"))
-        opts (merge-options project opts)
-        write (if (:pprint opts) 'clojure.pprint/pprint `prn)
-        result (gensym "result")
-        run-form `(jmh.core/run '~env ~(options-form opts))]
-
-    (when-not (seq (:benchmarks env))
-      (main/abort "No benchmarks found"))
-
-    `(try
-       (let [~result ~(if (:only opts)
-                        `(for [m# ~run-form]
-                           (select-keys m# '~(:only opts)))
-                        run-form)]
-         ~(if (string? out)
-            `(spit ~out (with-out-str (~write ~result)))
-            `(binding [*out* ~(if (= :err out) `*err* `*out*)]
-               (~write ~result))))
-       (catch Exception e#
-         (.printStackTrace e#)
-         (System/exit 1)))))
-
-(def ^{:private true
-       :doc "The profiling report code."}
-  profilers-form
-  `(let [profilers# (->> (jmh.core/profilers)
-                         (filter :supported)
-                         (sort-by :name))
-         keys# [:name :desc]
-         widths# (for [k# keys#
-                       :let [lens# (map (comp count str k#)
-                                        profilers#)]]
-                   (apply max (cons (count (str k#)) lens#)))
-         fmts# (map #(format "~%da" %) widths#)
-         fmt# (str "~:{~%"
-                   (apply str (interpose "  " fmts#))
-                   "~}")
-         seps# (for [w# widths#]
-                 (apply str (repeat w# \-)))
-         header# [keys# seps#]]
-     (->> profilers#
-          (map (apply juxt keys#))
-          (concat header#)
-          (clojure.pprint/cl-format true fmt#))))
 
 (defn jmh
   "Benchmark with JMH, the Java Microbenchmark Harness.
@@ -177,7 +79,16 @@ configuration and options."
          project (project/merge-profiles project [{:dependencies deps}])
 
          form (if (= :profilers task-arg)
-                profilers-form
-                (run-form project task-arg))]
+                (let [rows `(->> (jmh.core/profilers)
+                                 (filter :supported)
+                                 (sort-by :name))]
+                  (form/print-table [:name :desc] rows))
+                (let [opts (merge-options project task-arg)
+                      file (:file opts "jmh.edn")
+                      env (merge-environment project file)]
+                  (when-not (or (seq (:benchmarks env))
+                                (seq (:externs opts)))
+                    (main/abort "No benchmarks found"))
+                  (form/run-benchmarks env opts)))]
 
-     (eval/eval-in-project project form init-form))))
+     (eval/eval-in-project project form form/init))))
