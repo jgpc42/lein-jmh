4950c8984f377b16df81f95930ee75453c3323cf
Justin Conklin <jgpc42@gmail.com>
2017-10-02 20:55:31 -0700

    additions to the sample project and additional tests

--- test/leiningen/test/jmh.clj
+++ test/leiningen/test/jmh.clj
@@ -2,7 +2,20 @@
   (:require [leiningen.jmh :as jmh]
             [leiningen.test-util :as util]
             [clojure.edn :as edn]
+            [clojure.string :as str]
             [clojure.test :refer :all]))
+
+(defn run-task [& [arg :as args]]
+  (if (map? arg)
+    (let [temp (doto (java.io.File/createTempFile "temp" ".edn")
+                 .deleteOnExit)
+          opts (->> (assoc arg :output (str temp))
+                    (merge {:fail-on-error true, :warnings false}))
+          streams (util/run-task-in-project "sample-project" jmh/jmh [opts])]
+      (assoc streams :result (slurp temp)))
+    (util/run-task-in-project "sample-project" jmh/jmh [arg])))
+
+;;;
 
 (deftest test-merge-recursively
   (is (= {:a [1 5], :b {:c 6, :d #{3 7}}, :e 4, :f 8}
@@ -11,22 +24,38 @@
           {:a [5], :b {:c 6, :d #{7}}, :f 8}))))
 
 (deftest ^:integration test-jmh
-  (let [temp (doto (java.io.File/createTempFile "temp" ".edn")
-               .deleteOnExit)
-        opts {:fail-on-error true
-              :output (str temp)
-              :status true
-              :warnings false}
-        result (util/run-task-in-project "sample-project" jmh/jmh [opts])
-        output (slurp temp)]
+  (testing "default format"
+    (let [{result :result} (run-task {})]
+      (is (= [\( \{]
+             (take 2 result))
+          (str "invalid output:\n" (pr-str result)))
+      (is (= [0 1] (->> result edn/read-string (map :index))))))
 
-    (is (= [\( \{]
-           (take 2 output))
-        (str "invalid output:\n" (pr-str output)))
+  (testing "status"
+    (let [{out :out} (run-task {:status true})]
+      (is (re-find #"(?m)^# JMH version: [.\d]+$"
+                   out))
+      (is (re-find #"(?m)^# Run complete. Total time: [:\d]+$"
+                   out))))
 
-    (is (-> output edn/read-string first :samples))
+  (testing "profilers"
+    (is (re-find #"^:name +:desc +\n----"
+                 (:out (run-task :profilers)))))
 
-    (is (re-find #"(?m)^# JMH version: [.\d]+$"
-                 (:out result)))
-    (is (re-find #"(?m)^# Run complete. Total time: [:\d]+$"
-                 (:out result)))))
+  (testing "sort"
+    (is (= [1 0] (->> (run-task {:sort [:index :desc]})
+                      :result edn/read-string (map :index)))))
+
+  (testing "keys"
+    (let [all #{:args :samples :fn :index :name
+                :mode :params :threads :score}]
+      (are [s m] (= s (->> (run-task m) :result edn/read-string
+                           (mapcat keys) set))
+        all                {}
+        (disj all :score)  {:exclude :score}
+        #{:score :threads} {:only [:foo :score :threads]}
+        #{:threads}        {:exclude [:foo :score], :only [:score :threads]})))
+
+  (testing "table format"
+    (is (re-find #"^:fn/method .+\n----"
+                 (-> (run-task {:format :table}) :result str/trim)))))
